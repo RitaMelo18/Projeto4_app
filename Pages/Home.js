baf7b1f9e629821f56aa644d546c1900050e5ea4
RitaMelo18 <meloana@ipvc.pt>
2021-04-21 00:31:42 +0100

    Primeiro CardView

--- Pages/Home.js
+++ Pages/Home.js
@@ -1,18 +1,19 @@
 import * as React from 'react';
-import { Button, View, Text } from 'react-native';
+import { StyleSheet, Button, View, Text, TouchableOpacity, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import CozidoScreen from './HomePages/Cozido'
 
 function HomeScreen({ navigation }) {
+    let { container, cardText, card, cardImage } = styles
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <View style={container}>
+            <TouchableOpacity style={card} onPress={() => navigation.navigate("Cozido")}>
+                <Image style={cardImage} source={require('../images/logo.jpeg')}/>
+                <Text style={cardText}>Card Title</Text>
+            </TouchableOpacity>
             <Text>Página Inicial!</Text>
-            <Button
-                title="Go to Cozido eheh"
-                onPress={() => navigation.navigate("Cozido")}
-            />
         </View>
     );
 }
@@ -30,5 +31,37 @@ function App() {
         </NavigationContainer>
     );
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    cardText: {
+        fontSize: 30
+    },
+    card :{
+        backgroundColor: '#fff',
+        marginBottom: 10,
+        marginLeft: '2%',
+        width: '96%',
+        shadowColor: '#000',
+        shadowOpacity: 1,
+        shadowOffset :{
+            width: 3,
+            height: 3
+        }
+    },
+    cardImage :{
+        width: '100%',
+        height: 200,
+        resizeMode: 'cover'
+    }
+
+
+})
+
 
 export default App;
