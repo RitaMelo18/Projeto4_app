bc04e5d32b84c33e9bc696e49a7090d6fd6d1f36
RitaMelo18 <meloana@ipvc.pt>
2021-04-21 18:12:29 +0100

    Página cozido - Get exemplo feito

--- App.js
+++ App.js
@@ -72,3 +72,4 @@ const TabNavigator = createBottomTabNavigator(
 );
 
 export default createAppContainer(TabNavigator);
+
--- Pages/HomePages/Cozido.js
+++ Pages/HomePages/Cozido.js
@@ -1,13 +1,44 @@
-import React from 'react';
-import { Text, View} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, FlatList, Text, View } from 'react-native';
+
 
 function CozidoScreen() {
+    const [isLoading, setLoading] = useState(true);
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch('https://22059cm.000webhostapp.com/myslim/api/utilizador')
+            .then((response) => response.json())
+            .then((json) => setData(json))
+            .catch((error) => console.error(error))
+            .finally(() => setLoading(false));
+    });
+
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Cozido</Text>
+        <View style={{ flex: 1, padding: 24 }}>
+            {isLoading ? <ActivityIndicator /> : (
+                <FlatList
+                    data={data}
+                    keyExtractor={({ id }, index) => id}
+                    renderItem={({ item }) => (
+                        <Text>Id: {item.id}, Email: {item.email}</Text>
+                    )}
+                />
+            )}
         </View>
-
     );
-}
+};
 
-export default CozidoScreen;
+
+
+
+    // function CozidoScreen() {
+    //     return (
+    //         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    //             <Text>Cozido</Text>
+    //         </View>
+
+    //     );
+    // }
+
+    export default CozidoScreen;
