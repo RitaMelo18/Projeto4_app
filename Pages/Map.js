edbeed1f6cf69f81b0052cd50706ac3eaa978e3a
RitaMelo18 <meloana@ipvc.pt>
2021-05-12 16:56:31 +0100

    Listar pontos e dados dos mesmos

--- Pages/Map.js
+++ Pages/Map.js
@@ -1,13 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import React, { useEffect, useState, Component } from 'react';
+import { Text, View, StyleSheet, PermissionsAndroid, Alert } from 'react-native';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import { createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
-import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps'
+import MapView, { PROVIDER_GOOGLE, Marker, Callout } from 'react-native-maps'
 import Geolocation from '@react-native-community/geolocation';
+import { color } from 'react-native-reanimated';
+
 
 function MapScreen() {
     const [error, setError] = useState();
+    const [loading, setLoading] = useState(true);
+    const [data, setData] = useState([]);
+
     const [initialPosition, setInicialPosition] = useState({
         latitude: 41.6946,
         longitude: -8.83016,
@@ -37,6 +42,16 @@ function MapScreen() {
     };
 
     useEffect(() => {
+        fetch('http://apibackoffice.confrariadocozido.pt/api/get/restaurantes')
+            .then((response) => response.json())
+            .then((responseJson) => {
+                setLoading(false),
+                    setData(responseJson)
+            })
+            .catch((error) => console.error(error))
+    }, []);
+
+    useEffect(() => {
         Geolocation.getCurrentPosition(handleSuccess, handleError);
     }, []);
 
@@ -46,31 +61,55 @@ function MapScreen() {
     }, []);
 
     return (
-    <View style={styles.container}>
-        <MapView
-            provider={PROVIDER_GOOGLE}
-            style={styles.map}
-            region={initialPosition}
-        >
-        <Marker
-            coordinate={markerPosition}
-        ></Marker>    
-        </MapView>
-    </View>
-        // <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        //     <Text>Mapa!</Text>
-        // </View>
+        <View style={styles.container}>
+            <MapView
+                provider={PROVIDER_GOOGLE}
+                style={styles.map}
+                region={initialPosition}
+            >
+                <Marker
+                    coordinate={markerPosition}
+                    pinColor='blue'
+                ></Marker>
+
+                {data.map(marker => (
+                    <Marker
+                        key={marker.id}
+                        coordinate={{ latitude: Number(marker.latitude), longitude: Number(marker.longitude) }}
+                        pinColor='#44753d'
+                    >
+                        <Callout onPress={Callout}>
+                            <Text  style={styles.title}>{marker.nome}</Text>
+                            <Text>Morada: {marker.morada}</Text>
+                            <Text>Contacto: {marker.contacto}</Text>
+                            <Text>Dia do Cozido: {marker.diaCozido}</Text>
+                            <Text>Horário: {marker.horaInicio}h - {marker.horaFim}h</Text>
+                            <Text>Preço: {marker.precoMinimo}€ - {marker.precoMaximo}€</Text>
+                        </Callout>
+                        {console.log(marker.latitude)}
+                    </Marker>
+                ))}
+            </MapView>
+        </View>
     );
+
 }
 
+
 const styles = StyleSheet.create({
-    container:{
+    container: {
         ...StyleSheet.absoluteFillObject,
         height: '100%',
     },
-    map:{
+    map: {
         ...StyleSheet.absoluteFillObject,
     },
+    title: {
+        fontSize:18,
+        textAlign: 'center',
+        fontWeight: 'bold',
+    },
 })
+
 
 export default MapScreen;
