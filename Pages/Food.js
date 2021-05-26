286a6e799a4495955e73f90bf6bbe1798e176138
Hélder Gonçalves <helderpimentaxc@gmail.com>
2021-05-26 22:53:08 +0100

    ios

--- Pages/Food.js
+++ Pages/Food.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import {Text, View} from 'react-native';
-import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
-import {createAppContainer} from 'react-navigation';
-import {createBottomTabNavigator} from 'react-navigation-tabs';
 
 function FoodScreen() {
   return (
