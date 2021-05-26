76e4946245f34558b838a608f25086820612da0d
RitaMelo18 <meloana@ipvc.pt>
2021-05-26 10:41:24 +0100

    Página Créditos

--- Pages/HomePages/Creditos.js
+++ Pages/HomePages/Creditos.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Text, View, Image } from 'react-native';
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen'; //Ecrã responsivo
 
 function CreditosScreen() {
     const [didMount, setDidMount] = useState(false);
@@ -16,8 +17,15 @@ function CreditosScreen() {
     return (
         <View style={{ flex: 1, padding: 24 }}>
             <View >
-                <Image style={{ width: 350, height: 250, resizeMode: 'contain' }} source={require('../../images/creditos2.png')} />
-                <Text style={{textAlign: 'justify'}}>TEXTO</Text>
+                <Image style={{width:wp('90%'), height:hp('35%'), resizeMode:'contain', justifyContent: 'center', alignItems: 'center'}} source={require('../../images/creditos2.png')} />
+                <Text style={{textAlign: 'justify', lineHeight: 20}}>
+                    <Text style={{fontWeight: 'bold', fontSize: 16}}>{`Equipa de Desenvolvimento\n`}</Text>
+                    <Text style={{fontSize: 14}}>{`Ana Rita Melo\nJoão Lopes\n\n`}</Text>
+                    <Text style={{fontWeight: 'bold', fontSize: 16}}>{`Equipa de Publicação\n`}</Text>
+                    <Text style={{fontSize: 14}}>{`Ana Rita Melo\nJoão Lopes\nHélder Gonçalves\n\n`}</Text>
+                    <Text style={{fontWeight: 'bold', fontSize: 16}}>{`Supervisão Técnica\n`}</Text>
+                    <Text style={{fontSize: 14}}>{`Sara Paiva`}</Text>
+                </Text>
             </View>
         </View>
 
