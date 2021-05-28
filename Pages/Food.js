5257100f2f6d6edbf242c2106fedf3a4a90ee5c0
RitaMelo18 <meloana@ipvc.pt>
2021-05-29 00:25:53 +0100

    pagina restaurantes - pesquisa feita

--- Pages/Food.js
+++ Pages/Food.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Text, View, StyleSheet, FlatList, Image, TouchableOpacity } from 'react-native';
+import { Text, View, StyleSheet, FlatList, Image, TouchableOpacity, SafeAreaView } from 'react-native';
 import Icon from 'react-native-ionicons';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
@@ -12,124 +12,145 @@ import {
 } from 'react-native-responsive-screen'; //Ecrã responsivo
 import { Platform } from 'react-native';
 
-function FoodScreen() {
-    const [isLoading, setLoading] = useState(true);
-    const [data, setData] = useState([]);
-    const [RegiaoSelecionada, setRegiaoSelecionada] = useState([]);
-    const [didMount, setDidMount] = useState(false);
+
+
+const FoodScreen = () => {
+    const [search, setSearch] = useState('');
+    const [filteredDataSource, setFilteredDataSource] = useState([]);
+    const [masterDataSource, setMasterDataSource] = useState([]);
 
     useEffect(() => {
         fetch('http://apibackoffice.confrariadocozido.pt/api/get/regioesRestaurantes')
-            .then(response => response.json())
-            .then(json => setData(json))
-            .catch(error => console.error(error))
-            .finally(() => setLoading(false));
-    });
+            .then((response) => response.json())
+            .then((responseJson) => {
+                setFilteredDataSource(responseJson);
+                setMasterDataSource(responseJson);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }, []);
 
-    useEffect(() => {
-        setDidMount(true);
-        return () => setDidMount(false);
-      }, []);
-    
-      if (!didMount) {
-        return null;
-      }
-    return (
-        <View>
-            <Text></Text>
-            <Text style={styles.info}>{`Caso queira aderir e ser recomendado contacte:\nXXXXXXXXX`}</Text>
+    const searchFilterFunction = (text) => {
+        // Check if searched text is not blank
+        if (text) {
+            // Inserted text is not blank
+            // Filter the masterDataSource
+            // Update FilteredDataSource
+            const newData = masterDataSource.filter(function (item) {
+                const itemData = item.nome
+                    ? item.nome.toUpperCase()
+                    : ''.toUpperCase();
+                const textData = text.toUpperCase();
+                return itemData.indexOf(textData) > -1;
+            });
+            setFilteredDataSource(newData);
+            setSearch(text);
+        } else {
+            // Inserted text is blank
+            // Update FilteredDataSource with masterDataSource
+            setFilteredDataSource(masterDataSource);
+            setSearch(text);
+        }
+    };
+
+    const ItemView = ({ item }) => {
+        return (
+            // Flat List Item
             <View>
+                <Image
+                    style={styles.cardImage}
+                    source={{ uri: 'http://backoffice.confrariadocozido.pt/' + item.imagem }}
+                />
+                <Text style={styles.cardText} onPress={() => getItem(item)}>
+                    {item.nome.toUpperCase()}
+                </Text>
+                <Text></Text>
+            </View>
+
+        );
+    };
+
+    const ItemSeparatorView = () => {
+        return (
+            // Flat List Item Separator
+            <View
+                style={{
+                    height: 0.5,
+                    width: '100%',
+                    backgroundColor: '#C8C8C8',
+                }}
+            />
+        );
+    };
+
+    const getItem = (item) => {
+        // Function for click on an item
+        alert('Id : ' + item.id + ' Title : ' + item.nome);
+    };
+
+    return (
+        <SafeAreaView style={{ flex: 1, backgroundColor: '#FFF' }}>
+            <View style={styles.container}>
+                <Text></Text>
+                <Text style={styles.info}>{`Caso queira aderir e ser recomendado contacte:\nXXXXXXXXX`}</Text>
                 <SearchBar
-                    round={true}
-                    lightTheme={true}
                     platform='android'
-                    // placeholder="Search"
-                    icon={() => <FontAwesome5 class="far fa-search" size={30} color={"#FFF"} />}
+                    searchIcon={{ size: 24 }}
+                    onChangeText={(text) => searchFilterFunction(text)}
+                    onClear={(text) => searchFilterFunction('')}
+                    placeholder="Procure uma Região..."
+                    value={search}
                 />
-            </View>
-            <View style={styles.container, styles.card}>
                 <FlatList
-                    style={{ height: hp('100%') }}
-                    data={data}
-                    keyExtractor={({ id }, index) => id}
-                    renderItem={({ item }) => (
-                        <View style={{ flex: 1, flexDirection: 'row', backgroundColor: '#FFF' }}>
-                            <TouchableOpacity  onPress={() => {setRegiaoSelecionada(item.nome), console.log(RegiaoSelecionada)
-
-                            }}>
-                                <Image
-                                    style={styles.cardImage}
-                                    source={{ uri: 'http://backoffice.confrariadocozido.pt/' + item.imagem }}
-                                />
-                                <Text style={styles.cardText}>
-                                    {item.nome}
-                                </Text>
-                                <Text></Text>
-                            </TouchableOpacity>
-                        </View>
-                    )}
+                    data={filteredDataSource}
+                    keyExtractor={(item, index) => index.toString()}
+                    ItemSeparatorComponent={ItemSeparatorView}
+                    renderItem={ItemView}
                 />
-
-
-
             </View>
-
-
-        </View>
+        </SafeAreaView>
     );
-}
+};
 
 const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'white',
+        alignContent: 'center',
+        alignItems: 'center'
+    },
+    itemStyle: {
+        padding: 10,
+        alignContent:'center',
+        alignItems:'center'
+    },
     info: {
-        width: 375,
-        height: 78,
+        width: wp('90%'),
+        height: hp('10%'),
         borderColor: '#44753d',
         borderWidth: 1,
         borderRadius: 10,
         padding: 20,
         justifyContent: 'center',
         alignItems: 'center',
-        left: 10,
-        right: 10,
+        left: 5,
+        right: 5,
         textAlign: 'center',
         fontSize: 14,
         lineHeight: 20
-    },
-    container: {
-        marginTop: 30,
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    cardText: {
-        fontSize: 20,
-        alignItems: 'center',
-        justifyContent: 'center',
-        alignContent: 'center',
-        textAlign: 'center',
-    },
-    card: {
-        backgroundColor: '#fff',
-        marginBottom: 10,
-        marginLeft: '2%',
-        marginRight: '2%',
-        width: wp('95%'),
-        marginTop: 5,
-        height: hp('100%'),
-        shadowColor: '#000',
-        shadowOpacity: 1,
-        shadowOffset: {
-            width: 5,
-            height: 5,
-        },
     },
     cardImage: {
         width: wp('95%'),
         height: hp('20%'),
         resizeMode: 'cover',
+    },
+    cardText: {
+        fontSize: 16,
+        alignItems: 'center',
+        justifyContent: 'center',
+        alignContent: 'center',
+        textAlign: 'center',
     }
-
-})
-
+});
 
 export default FoodScreen;
